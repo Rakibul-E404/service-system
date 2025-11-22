908e24fea1cabde3002962b17aeb96b9a82c0d63
Rakibul-E404 <smrat246@gmail.com>
2025-11-23 01:24:07 +0600

    profile update delay- bug fixed

--- lib/features/home/bindings/home_binding.dart
+++ lib/features/home/bindings/home_binding.dart
@@ -1,5 +1,41 @@
+//
+// import 'package:get/get.dart';
+// import '../controllers/home_top_bar_controller.dart';
+// import '../controllers/notification_controller.dart';
+// import '../controllers/provider_details_controller.dart';
+// import '../controllers/sub_categories_controller.dart';
+// import '../controllers/home_controller.dart';
+// import '../controllers/home_service_details_controller.dart';
+// import '../controllers/search_controller.dart';
+//
+// class HomeBinding extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.lazyPut<HomeController>(() => HomeController());
+//     Get.lazyPut<HomeSearchController>(() => HomeSearchController());
+//     Get.lazyPut<HomeServiceDetailsController>(() => HomeServiceDetailsController());
+//     Get.lazyPut<SubCategoriesController>(() => SubCategoriesController());
+//     Get.lazyPut<ProviderDetailsController>(() => ProviderDetailsController());
+//     Get.lazyPut(() => NotificationController());
+//     Get.lazyPut<HomeTopBarController>(() => HomeTopBarController());
+//
+//   }
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
 
 import 'package:get/get.dart';
+import '../../profile/controllers/profile_controller.dart'; // Import ProfileController
 import '../controllers/notification_controller.dart';
 import '../controllers/provider_details_controller.dart';
 import '../controllers/sub_categories_controller.dart';
@@ -17,15 +53,8 @@ class HomeBinding extends Bindings {
     Get.lazyPut<ProviderDetailsController>(() => ProviderDetailsController());
     Get.lazyPut(() => NotificationController());
 
-
+    // Use ProfileController instead of HomeTopBarController
+    // This ensures the same controller instance is used across the app
+    Get.lazyPut<ProfileController>(() => ProfileController(), fenix: true);
   }
 }
-
-
-
-
-
-
-
-
-
