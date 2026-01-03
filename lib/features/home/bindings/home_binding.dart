0a7b4182f9b8a2dd1a2438bb15115baa6b64839b
Rakibul-E404 <smrat246@gmail.com>
2026-01-03 15:47:03 +0600

    search screen and the all service screen updated__

--- lib/features/home/bindings/home_binding.dart
+++ lib/features/home/bindings/home_binding.dart
@@ -1,39 +1,3 @@
-//
-// import 'package:get/get.dart';
-// import '../controllers/home_top_bar_controller.dart';
-// import '../controllers/notification_controller.dart';
-// import '../controllers/provider_details_controller.dart';
-// import '../controllers/sub_categories_controller.dart';
-// import '../controllers/home_controller.dart';
-// import '../controllers/home_service_details_controller.dart';
-// import '../controllers/search_controller.dart';
-//
-// class HomeBinding extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut<HomeController>(() => HomeController());
-//     Get.lazyPut<HomeSearchController>(() => HomeSearchController());
-//     Get.lazyPut<HomeServiceDetailsController>(() => HomeServiceDetailsController());
-//     Get.lazyPut<SubCategoriesController>(() => SubCategoriesController());
-//     Get.lazyPut<ProviderDetailsController>(() => ProviderDetailsController());
-//     Get.lazyPut(() => NotificationController());
-//     Get.lazyPut<HomeTopBarController>(() => HomeTopBarController());
-//
-//   }
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
-
 import 'package:get/get.dart';
 import '../../profile/controllers/profile_controller.dart'; // Import ProfileController
 import '../controllers/notification_controller.dart';
